0e44d555060db77b053cab907710f1dd3e7bff11
derickard <derickard@gmail.com>
2020-06-11 13:55:33 -0400

    models

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
@@ -27,10 +30,27 @@ namespace CodingEvents.Controllers
         [HttpPost("/Events/Add")]
         public IActionResult NewEvent(string name, string description)
         {
-            Events.Add(name, description);
-            //Events[name] = description;
+            EventData.Add(new Event(name, description));
+
             return Redirect("/Events");
         }
 
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach(int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
     }
 }
---
+++ CodingEvents/Models/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Models
+{
+    public class Event
+    {
+        public int Id { get; }
+        static private int nextId = 1;
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Event(string name, string description) 
+        {
+            Name = name;
+            Description = description;
+            Id = nextId;
+            nextId++;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+
+    }
+}
